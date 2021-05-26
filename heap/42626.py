ac841f58051414fdedcfa6536e6d6d14762a7052
2014031930 <inseo8070@naver.com>
2021-05-26 09:51:09 +0900

    42626.py

---
+++ heap/42626.py
@@ -0,0 +1,19 @@
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    heap = []
+    for i in scoville:
+        heapq.heappush(heap, i)
+        
+    while heap[0] < K:
+        a = heapq.heappop(heap)
+        b = heapq.heappop(heap)
+        new_scoville = a + (b * 2)
+        heapq.heappush(heap, new_scoville)
+        answer += 1
+        if len(heap) == 1 and heap[0] < K:
+            answer = -1
+            break
+
+    return answer
