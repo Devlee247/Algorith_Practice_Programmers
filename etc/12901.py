f9366afc9ab610a2bffc123f6f869ba4defead33
Devlee247 <inseo8070@naver.com>
2021-06-23 21:30:54 +0900

    12901.py

--- etc/12901.py
+++ etc/12901.py
@@ -2,7 +2,6 @@ def solution(a, b):
     answer = ''
     month_day = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     day = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"]
-    
     days = b-1
     
     for month in range(1, a):
